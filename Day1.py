e24a1ace3ab8182d1bbe31bc51d44190815e2e6a
Joshua Robinson <34900580+CodesLikeChicken@users.noreply.github.com>
2023-10-17 16:14:19 +0000

    Add day 1.

---
+++ Day1.py
@@ -0,0 +1,18 @@
+# Day 1 is focused on input and output
+
+#1. Create a greeting for your program.
+print("Good morning / day / night sir / madam")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What is the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of a pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your unimaginative band name is...")
+print(city + pet)
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
