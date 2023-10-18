d4570d8d7c9fc41cd140ab2b0171078c14520669
Joshua Robinson <34900580+CodesLikeChicken@users.noreply.github.com>
2023-10-18 05:01:32 +0000

    Ensure leap year flag is always set.

--- day3.LeapYear.py
+++ day3.LeapYear.py
@@ -6,13 +6,19 @@
 
 year = int(input())
 
-is_leap_year = False
-
 if year % 4 == 0:
     if year % 100 == 0 and not year % 400 == 0:
         is_leap_year = False
     else:
         is_leap_year = True
+else:
+    is_leap_year = False
+
+
+# !!! Python quirk!  is_leap_year is not scoped to the if statement above.
+# They are scoped to the innermost function, class, or module
+# In this case, is_leap_year is considered part of the global scope so 
+# it is available here with the values set in the if statement.
 
 if is_leap_year:
     print ("Leap year")
