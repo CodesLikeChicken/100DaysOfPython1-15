819eb5286dcaefacce50ab97206c1bc1cb839902
Joshua Robinson <34900580+CodesLikeChicken@users.noreply.github.com>
2023-10-18 04:11:19 +0000

    Code clean up.

--- Day2.TipCalculator.py
+++ Day2.TipCalculator.py
@@ -9,11 +9,10 @@
 
 print("Welcome to the tip calculator.")
 total_bill = input("What was the total bill? ")
-tip_percent = input(
-    "What percentage tip would you like ot give? 10, 12, or 15? ")
+tip_percent = input("What percentage tip would you like ot give? 10, 12, or 15? ")
 split_size = input("How many people to split the bill? ")
 
 tip_percent = float(tip_percent) / 100
-total_bill = float(total_bill) * (1 + tip_percent) / int(split_size)
+bill_per_person = float(total_bill) * (1 + tip_percent) / int(split_size)
 
-print(f"Each person should pay ${total_bill:.2f}.")
+print(f"Each person should pay ${bill_per_person:.2f}.")
