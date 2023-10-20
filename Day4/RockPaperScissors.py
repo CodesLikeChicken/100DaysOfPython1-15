ddface367e453504f8deabcb420e4022132aa83b
Joshua Robinson <34900580+CodesLikeChicken@users.noreply.github.com>
2023-10-20 04:30:56 +0000

    Fix Rock Paper scissor error checking.

--- Day4/RockPaperScissors.py
+++ Day4/RockPaperScissors.py
@@ -35,13 +35,15 @@ if user_choice >= len(game_images):
     print("Invalid choice.  You lose!")
     exit()
 
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+  exit()
+
 print(game_images[user_choice])
 
 computer_choice = random.randint(0, 2)
 
-if user_choice >= 3 or user_choice < 0: 
-  print("You typed an invalid number, you lose!") 
-elif user_choice == 0 and computer_choice == 2:
+if user_choice == 0 and computer_choice == 2:
   print("You win!")
 elif computer_choice == 0 and user_choice == 2:
   print("You lose")
