c4693bb689e29476127382d1bdded5fb7526c2fd
Joshua Robinson <34900580+CodesLikeChicken@users.noreply.github.com>
2023-11-03 06:12:13 +0000

    Combine common code.

--- Day8/CaesarCipher.py
+++ Day8/CaesarCipher.py
@@ -2,39 +2,24 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift):
+def caesar(text, shift, direction):
+    if direction == "decode":
+        shift = shift * -1
+    elif not direction == "encode":
+        print("Invalid direction")
+        return 
     
-    encoded_text = ""
+    cyphered_text = ""
 
     first_letter_ord = ord('a')
 
-    for text_index in range(len(plain_text)):
-        orig_char_ord = ord(plain_text[text_index])
+    for text_index in range(len(text)):
+        orig_char_ord = ord(text[text_index])
         original_letter_index = orig_char_ord - first_letter_ord
         new_letter_index = (original_letter_index + shift) % 26
         new_char_ord = new_letter_index + first_letter_ord
-        encoded_text += chr(new_char_ord)
+        cyphered_text += chr(new_char_ord)
 
-    print(f"The encoded text is {encoded_text}")
+    print(f"The {direction} text is {cyphered_text}")
 
-def decrypt(encoded_text, shift):
-    
-    plain_text = ""
-
-    first_letter_ord = ord('a')
-
-    for text_index in range(len(encoded_text)):
-        orig_char_ord = ord(encoded_text[text_index])
-        original_letter_index = orig_char_ord - first_letter_ord
-        new_letter_index = (original_letter_index - shift) % 26
-        new_char_ord = new_letter_index + first_letter_ord
-        plain_text += chr(new_char_ord)
-
-    print(f"The encoded text is {plain_text}")
-
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
-else:
-    print("Invalid direction")
+caesar(text, shift, direction)
